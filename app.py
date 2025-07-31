32c675a8e330cdcc4b116c1911c31303866b55b9
PAKSHE <bheemkekak@gmail.com>
2025-07-31 23:41:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +12,4 @@ def next_page():
     return render_template('next.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+ app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
